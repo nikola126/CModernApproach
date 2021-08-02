26dd4b8978ff964d97fd2eb1ea83656c7785f9c3
Nikola126 <nikj1705@gmail.com>
2021-08-02 13:43:07 +0300

    renaming

--- Chapter12/Chapter12_Project1.c
+++ Chapter12/Chapter12_Project1.c
@@ -7,22 +7,22 @@ void reverse_message_int()
 {
     char message[MAX_LEN];
     char c;
-    int mes_len = 0;
+    int current_pos = 0;
     printf("Enter a message: \n");
     while (true)
     {
         c = getchar();
         if (c == '\n')
             break;
-        if (mes_len >= MAX_LEN)
+        if (current_pos >= MAX_LEN)
             break;
-        message[mes_len] = c;
-        mes_len += 1;
+        message[current_pos] = c;
+        current_pos += 1;
     }
-
-    printf("Using array indexing: \n");
-    for (int i = mes_len - 1; i >= 0; i--)
+    for (int i = current_pos - 1; i >= 0; i--)
+    {
         printf("%c", message[i]);
+    }
     printf("\n");
 }
 
@@ -45,7 +45,7 @@ void reverse_message_pointer()
     }
 
     printf("Using pointer arithmetic: \n");
-    for (current_pos = message + mes_len; current_pos >= message; current_pos--)
+    for (current_pos = message + mes_len - 1; current_pos >= message; current_pos--)
         printf("%c", *current_pos);
     printf("\n");
 }
